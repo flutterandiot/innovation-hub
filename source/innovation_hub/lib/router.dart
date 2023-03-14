b8389790f3e153fcc1dd5b5e65b648602eb24c6d
Dantopia <dantopiavn@gmail.com>
2023-03-14 08:52:18 +0700

    can go ShellRoute, however, the primaryNavi still have janky issue, it display not abnormal

--- source/innovation_hub/lib/router.dart
+++ source/innovation_hub/lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:innovation_hub/app/dashboard/dashboard_body.dart';
 import 'package:innovation_hub/app/home/home_page.dart';
 import 'package:innovation_hub/app/project/new_project_page.dart';
 
@@ -10,7 +11,7 @@ final _shellNavigatorKey = GlobalKey<NavigatorState>();
 
 final router = GoRouter(
   navigatorKey: _rootNavigatorKey,
-  initialLocation: '/',
+  initialLocation: '/dashboard',
   routes: [
     // GoRoute(
     //   path: '/',
@@ -25,13 +26,32 @@ final router = GoRouter(
     ShellRoute(
       navigatorKey: _shellNavigatorKey,
       builder: (context, state, child) {
-        return const HomePage();
+        return HomePage(body: child);
       },
       routes: [
         GoRoute(
-          path: 'new-project',
-          name: 'new-project',
-          builder: (context, state) => const NewProjectPage(),
+          path: '/dashboard',
+          name: 'dashboard',
+          builder: (context, state) => const DashboardPageBody(),
+          routes: [
+            GoRoute(
+              path: 'new-project',
+              name: 'new-project',
+              builder: (context, state) => const NewProjectPage(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/explore',
+          name: 'explore',
+          builder: (context, state) => const DashboardPageBody(),
+          routes: [
+            GoRoute(
+              path: 'learn-create-idea',
+              name: 'learn-create-idea',
+              builder: (context, state) => const NewProjectPage(),
+            ),
+          ],
         ),
       ],
     ),

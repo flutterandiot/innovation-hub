c7bf0808afb184ca4997ed891a70b2c144cf9d99
Dantopia <dantopiavn@gmail.com>
2023-03-22 14:15:24 +0700

    improve component management

--- source/innovation_hub/lib/app/project/widgets/list_components.dart
+++ source/innovation_hub/lib/app/project/widgets/list_components.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:textfield_tags/textfield_tags.dart';
 
-import 'package:innovation_hub/app/project/model/project_model.dart';
 import 'package:innovation_hub/app/project/provider/project_provider.dart';
 import 'package:innovation_hub/app/project/provider/step_provider.dart';
 import 'package:innovation_hub/utils/space.dart';
@@ -18,113 +17,43 @@ class ListComponents extends ConsumerStatefulWidget {
 }
 
 class _ListComponentsState extends ConsumerState<ListComponents> {
-  late TextfieldTagsController _tagController;
-  @override
-  void initState() {
-    _tagController = TextfieldTagsController();
-    super.initState();
-  }
+  final _internalTagController = TextfieldTagsController();
+  final _externalTagController = TextfieldTagsController();
 
   @override
   void dispose() {
-    _tagController.dispose();
+    _internalTagController.dispose();
+    _externalTagController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final currentProject = ref.watch(projectsProvider.notifier).currentProject;
-    ref.listen<int>(
-      currentStepProvider,
-      (previous, next) {
-        // If the go from list component, then save all component
-        if (previous == 1 && _tagController.hasTags) {
-          //Clear
-          currentProject!.internalComponents!.clear();
-          //Then copy all tags
-          currentProject.internalComponents!.addAll([...?_tagController.getTags]);
-        }
-      },
-    );
+    final currentProject = ref.watch(projectsProvider.notifier).currentProject!;
+
     return LayoutBuilder(builder: (context, constraints) {
-      final width = constraints.maxWidth;
       return Padding(
         padding: EdgeInsets.symmetric(
           horizontal: constraints.maxWidth * 0.2,
         ),
         child: Column(
           children: [
-            TextFieldTags(
-              textfieldTagsController: _tagController,
-              initialTags: [...currentProject!.internalComponents ?? List.empty()],
-              textSeparators: const [' ', ','],
-              letterCase: LetterCase.normal,
-              validator: (String tag) {
-                return null;
-                // if (tag == 'php') {
-                //   return 'No, please just no';
-                // } else if (_controller.getTags.contains(tag)) {
-                //   return 'you already entered that';
-                // }
-                // return null;
-              },
-              inputfieldBuilder: (context, tec, fn, error, onChanged, onSubmitted) {
-                return ((context, src, tags, onTagDelete) {
-                  return TextField(
-                    controller: tec,
-                    focusNode: fn,
-                    decoration: InputDecoration(
-                      isDense: true,
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 74, 137, 92),
-                          width: 3.0,
-                        ),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 74, 137, 92),
-                          width: 3.0,
-                        ),
-                      ),
-                      helperText: 'Enter internal components...',
-                      helperStyle: const TextStyle(
-                        color: Color.fromARGB(255, 74, 137, 92),
-                      ),
-                      hintText: _tagController.hasTags ? '' : "Enter component...",
-                      errorText: error,
-                      prefixIconConstraints: BoxConstraints(maxWidth: width * 0.4),
-                      prefixIcon: tags.isNotEmpty
-                          ? _TagListView(
-                              currentProject: currentProject,
-                              src: src,
-                              tags: tags,
-                              onTagDelete: onTagDelete,
-                            )
-                          : null,
-                    ),
-                    onChanged: onChanged,
-                    onSubmitted: (str) {
-                      onSubmitted!(str);
-                      currentProject.internalComponents!.add(str);
-                    },
-                  );
-                });
-              },
+            InputComponentTextField(
+              tagController: _internalTagController,
+              components: currentProject.internalComponents ?? [],
             ),
             Text(
               _internalComponentHelperText,
               style: Theme.of(context).textTheme.bodySmall,
             ),
             Space.y(32),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'List all external components',
-              ),
+            InputComponentTextField(
+              tagController: _externalTagController,
+              components: currentProject.externalComponents ?? [],
             ),
-            const Text(
+            Text(
               _externalComponentHelperText,
-              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.bodySmall,
             ),
           ],
         ),
@@ -133,16 +62,99 @@ class _ListComponentsState extends ConsumerState<ListComponents> {
   }
 }
 
+class InputComponentTextField extends ConsumerWidget {
+  const InputComponentTextField({
+    Key? key,
+    required this.tagController,
+    required this.components,
+  }) : super(key: key);
+
+  final TextfieldTagsController tagController;
+  final List<String> components;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<int>(
+      currentStepProvider,
+      (previous, next) {
+        // If the go from list component, then save all component
+        if (previous == 1 && tagController.hasTags) {
+          //Clear
+          components.clear();
+          //Then copy all tags
+          components.addAll([...?tagController.getTags]);
+        }
+      },
+    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return TextFieldTags(
+        textfieldTagsController: tagController,
+        initialTags: components,
+        textSeparators: const [','],
+        letterCase: LetterCase.normal,
+        validator: (String tag) {
+          return null;
+          // if (tag == 'php') {
+          //   return 'No, please just no';
+          // } else if (_controller.getTags.contains(tag)) {
+          //   return 'you already entered that';
+          // }
+          // return null;
+        },
+        inputfieldBuilder: (context, tec, fn, error, onChanged, onSubmitted) {
+          return ((context, src, tags, onTagDelete) {
+            return TextField(
+              controller: tec,
+              focusNode: fn,
+              decoration: InputDecoration(
+                isDense: true,
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 74, 137, 92),
+                    width: 3.0,
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 74, 137, 92),
+                    width: 3.0,
+                  ),
+                ),
+                helperStyle: const TextStyle(
+                  color: Color.fromARGB(255, 74, 137, 92),
+                ),
+                hintText: tagController.hasTags ? '' : "Enter component...",
+                errorText: error,
+                prefixIconConstraints: BoxConstraints(maxWidth: constraints.maxWidth * 0.5),
+                prefixIcon: tags.isNotEmpty
+                    ? _TagListView(
+                        src: src,
+                        tags: tags,
+                        onTagDelete: (mTag) {
+                          onTagDelete(mTag);
+                          components.remove(mTag);
+                        },
+                      )
+                    : null,
+              ),
+              onChanged: onChanged,
+              onSubmitted: onSubmitted,
+            );
+          });
+        },
+      );
+    });
+  }
+}
+
 class _TagListView extends StatelessWidget {
   const _TagListView({
     Key? key,
-    required this.currentProject,
     required this.src,
     required this.tags,
     required this.onTagDelete,
   }) : super(key: key);
 
-  final Project? currentProject;
   final ScrollController src;
   final List<String> tags;
   final Function(String) onTagDelete;
@@ -159,10 +171,7 @@ class _TagListView extends StatelessWidget {
           onTagSelect: (mTag) {
             debugPrint('Tag selected: $mTag');
           },
-          onTagDelete: (mTag) {
-            onTagDelete(mTag);
-            currentProject!.internalComponents!.remove(mTag);
-          },
+          onTagDelete: onTagDelete,
         );
       }).toList()),
     );

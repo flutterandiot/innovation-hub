c0dc2bea772ee38d3ee1d3d6dfeda01f974b0e51
Dantopia <dantopiavn@gmail.com>
2023-03-23 09:33:21 +0700

    to enable provider notify, call addProject from projectsProvider, add as normal list doesn't work

--- source/innovation_hub/lib/app/project/widgets/new_project_dialog.dart
+++ source/innovation_hub/lib/app/project/widgets/new_project_dialog.dart
@@ -145,7 +145,7 @@ class NewProjectDialog extends HookConsumerWidget {
     );
 
     //Save project to project list
-    ref.read(projectsProvider).add(project);
+    ref.read(projectsProvider.notifier).addProject(project);
     //NOTE: Save this new created project as a current ont
     ref.read(projectsProvider.notifier).setCurrentProject(project);
     debugPrint(project.toString());

a1c6327b52add371387557766e9584baee14d236
Dantopia <dantopiavn@gmail.com>
2023-03-23 21:37:44 +0700

    working on improve dashboard tabview

--- source/innovation_hub/lib/app/project/tab_views/dashboard/component_container.dart
+++ source/innovation_hub/lib/app/project/tab_views/dashboard/component_container.dart
@@ -8,12 +8,23 @@ class ProjectComponentsContainer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final components = ref.watch(activeProjectProvider.select((value) => value.components));
+    final internalComponent = [
+      for (final comp in components)
+        if (comp.isInternal) comp
+    ];
     return Container(
       decoration: BoxDecoration(
         border: Border.all(),
       ),
-      child: Center(
-        child: Text('Total: ${components.length}'),
+      child: Column(
+        children: [
+          Text(
+            'Total: ${components.length}',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+          Text('Internal: ${internalComponent.length}'),
+          Text('External: ${components.length - internalComponent.length}'),
+        ],
       ),
     );
   }

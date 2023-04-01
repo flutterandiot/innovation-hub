1920a29bc3ebb6d30088b164ac9d440718ec3082
Dantopia <dantopiavn@gmail.com>
2023-04-01 16:46:59 +0700

    delete ideaListContainer

--- source/innovation_hub/lib/app/project/project_views/techniques/sit_substractiion.dart
+++ source/innovation_hub/lib/app/project/project_views/techniques/sit_substractiion.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:innovation_hub/app/model/idea_model.dart';
@@ -281,112 +280,5 @@ class _ComponentListTile extends ConsumerWidget {
             ),
           );
         });
-  }
-}
-
-class _IdeaListViewContainer extends StatelessWidget {
-  const _IdeaListViewContainer();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 8, right: 8),
-      padding: const EdgeInsets.only(left: 8, right: 8),
-      decoration: BoxDecoration(
-          // border: Border.all(
-          //   color: Colors.grey,
-          // ),
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              blurRadius: 2,
-              color: Colors.black,
-            ),
-          ]),
-      height: 400,
-      width: double.infinity,
-      child: Column(
-        children: [
-          const Text('Ideas generated'),
-          Expanded(
-            child: Consumer(
-              builder: (context, ref, child) {
-                final ideas = ref.watch(ideasProvider);
-                return ListView.builder(
-                  itemCount: ideas.length,
-                  itemBuilder: (context, index) {
-                    return _IdeaListTile(ideas[index]);
-                    // _IdeaListTile(idea: ideas[index]);
-                  },
-                  // separatorBuilder: (context, index) {
-                  //   return const Divider();
-                  // },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _IdeaListTile extends HookConsumerWidget {
-  const _IdeaListTile(
-    this.idea,
-  );
-  final Idea idea;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final checked = useState(false);
-
-    debugPrint('Idea  rebuilt: ${idea.id}');
-    return Card(
-      child: ListTile(
-        // tileColor: Theme.of(context).primaryColor,
-        leading: Checkbox(
-          value: checked.value,
-          onChanged: (value) {
-            checked.value = value!;
-          },
-        ),
-        title: Text(idea.concept),
-        trailing: TextButton.icon(
-          onPressed: () {
-            _likeTheIdea(context, ref, idea);
-            // idea.rating++;
-          },
-          label: Text(
-            '${idea.rating}',
-            style: const TextStyle(color: Colors.red),
-          ),
-          icon: const Icon(
-            Icons.thumb_up,
-            color: Colors.blue,
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _likeTheIdea(BuildContext context, WidgetRef ref, Idea idea) {
-    // if (idea.rating < 5) {
-    //   ref.read(ideaManageProvider.select((mIdea) => mIdea!.rating++));
-    // }
-
-    ref.read(ideaManageProvider.notifier).ratingIdea(idea, 3);
-    // var message = '';
-    // if (result) {
-    //   message = "'${idea.concept}' has been added to project";
-    // } else {
-    //   message = "'${idea.concept}' is already added to project";
-    // }
-    // ScaffoldMessenger.of(context).clearSnackBars();
-    // ScaffoldMessenger.of(context).showSnackBar(
-    //   SnackBar(
-    //     content: Text(message),
-    //   ),
-    // );
   }
 }

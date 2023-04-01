279c0098c452183e477b463149a87709bbeac6bb
Dantopia <dantopiavn@gmail.com>
2023-04-01 16:46:11 +0700

    change provider to get the idea list

--- source/innovation_hub/lib/app/project/project_views/dashboard/ideas_container.dart
+++ source/innovation_hub/lib/app/project/project_views/dashboard/ideas_container.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:innovation_hub/app/provider/idea_controller.dart';
-import 'package:innovation_hub/app/provider/project_provider.dart';
 
 class IdeasContainer extends ConsumerWidget {
   const IdeasContainer({super.key});
@@ -18,9 +17,7 @@ class _IdeaListViewContainer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ideas = ref.watch(
-      activeProjectProvider.select((value) => value.ideas),
-    );
+    final ideas = ref.watch(ideasProvider);
     return Container(
       margin: const EdgeInsets.only(left: 8, right: 8),
       // padding: const EdgeInsets.only(left: 8, right: 8),

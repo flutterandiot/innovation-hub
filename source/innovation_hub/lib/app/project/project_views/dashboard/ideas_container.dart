75b62f820a05fe914dfc2b965149ee0e8d3892a9
Dantopia <dantopiavn@gmail.com>
2023-04-02 16:59:07 +0700

    add pluto_grid to study how to display a idea

--- source/innovation_hub/lib/app/project/project_views/dashboard/ideas_container.dart
+++ source/innovation_hub/lib/app/project/project_views/dashboard/ideas_container.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:innovation_hub/app/provider/idea_controller.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
 class IdeasContainer extends ConsumerWidget {
   const IdeasContainer({super.key});
@@ -57,23 +58,24 @@ class _IdeaListViewContainer extends ConsumerWidget {
           ),
           Expanded(
             child: (ideas.isNotEmpty)
-                ? ListView.builder(
-                    padding: const EdgeInsets.only(left: 8, right: 8),
-                    itemCount: ideas.length,
-                    itemBuilder: (context, index) {
-                      return ProviderScope(
-                        overrides: [
-                          ideaIndexProvider.overrideWithValue(index),
-                        ],
-                        child: const _IdeaListTile(),
-                      );
-                      // return _IdeaListTile(ideas[index]);
-                      // _IdeaListTile(idea: ideas[index]);
-                    },
-                    // separatorBuilder: (context, index) {
-                    //   return const Divider();
-                    // },
-                  )
+                ? const PlutoGridExamplePage()
+                // ? ListView.builder(
+                //     padding: const EdgeInsets.only(left: 8, right: 8),
+                //     itemCount: ideas.length,
+                //     itemBuilder: (context, index) {
+                //       return ProviderScope(
+                //         overrides: [
+                //           ideaIndexProvider.overrideWithValue(index),
+                //         ],
+                //         child: const _IdeaListTile(),
+                //       );
+                //       // return _IdeaListTile(ideas[index]);
+                //       // _IdeaListTile(idea: ideas[index]);
+                //     },
+                //     // separatorBuilder: (context, index) {
+                //     //   return const Divider();
+                //     // },
+                //   )
                 : const SizedBox(
                     child: Center(
                       child: Text("There isn't any idea for this for project."),
@@ -159,6 +161,123 @@ class _IdeaListTile extends HookConsumerWidget {
         onTap: () {
           ref.read(ideaManageProvider.notifier).setIdea(idea);
         },
+      ),
+    );
+  }
+}
+
+/// PlutoGrid Example
+//
+/// For more examples, go to the demo web link on the github below.
+class PlutoGridExamplePage extends StatefulWidget {
+  const PlutoGridExamplePage({Key? key}) : super(key: key);
+
+  @override
+  State<PlutoGridExamplePage> createState() => _PlutoGridExamplePageState();
+}
+
+class _PlutoGridExamplePageState extends State<PlutoGridExamplePage> {
+  final List<PlutoColumn> columns = <PlutoColumn>[
+    PlutoColumn(
+      title: 'Id',
+      field: 'id',
+      type: PlutoColumnType.text(),
+    ),
+    PlutoColumn(
+      title: 'Name',
+      field: 'name',
+      type: PlutoColumnType.text(),
+    ),
+    PlutoColumn(
+      title: 'Age',
+      field: 'age',
+      type: PlutoColumnType.number(),
+    ),
+    PlutoColumn(
+      title: 'Role',
+      field: 'role',
+      type: PlutoColumnType.select(<String>[
+        'Programmer',
+        'Designer',
+        'Owner',
+      ]),
+    ),
+    PlutoColumn(
+      title: 'Joined',
+      field: 'joined',
+      type: PlutoColumnType.date(),
+    ),
+    PlutoColumn(
+      title: 'Working time',
+      field: 'working_time',
+      type: PlutoColumnType.time(),
+    ),
+  ];
+
+  final List<PlutoRow> rows = [
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user1'),
+        'name': PlutoCell(value: 'Mike'),
+        'age': PlutoCell(value: 20),
+        'role': PlutoCell(value: 'Programmer'),
+        'joined': PlutoCell(value: '2021-01-01'),
+        'working_time': PlutoCell(value: '09:00'),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user2'),
+        'name': PlutoCell(value: 'Jack'),
+        'age': PlutoCell(value: 25),
+        'role': PlutoCell(value: 'Designer'),
+        'joined': PlutoCell(value: '2021-02-01'),
+        'working_time': PlutoCell(value: '10:00'),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'id': PlutoCell(value: 'user3'),
+        'name': PlutoCell(value: 'Suzi'),
+        'age': PlutoCell(value: 40),
+        'role': PlutoCell(value: 'Owner'),
+        'joined': PlutoCell(value: '2021-03-01'),
+        'working_time': PlutoCell(value: '11:00'),
+      },
+    ),
+  ];
+
+  /// columnGroups that can group columns can be omitted.
+  final List<PlutoColumnGroup> columnGroups = [
+    PlutoColumnGroup(title: 'Id', fields: ['id'], expandedColumn: true),
+    PlutoColumnGroup(title: 'User information', fields: ['name', 'age']),
+    PlutoColumnGroup(title: 'Status', children: [
+      PlutoColumnGroup(title: 'A', fields: ['role'], expandedColumn: true),
+      PlutoColumnGroup(title: 'Etc.', fields: ['joined', 'working_time']),
+    ]),
+  ];
+
+  /// [PlutoGridStateManager] has many methods and properties to dynamically manipulate the grid.
+  /// You can manipulate the grid dynamically at runtime by passing this through the [onLoaded] callback.
+  late final PlutoGridStateManager stateManager;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: PlutoGrid(
+          columns: columns,
+          rows: rows,
+          // columnGroups: columnGroups,
+          onLoaded: (PlutoGridOnLoadedEvent event) {
+            stateManager = event.stateManager;
+          },
+          onChanged: (PlutoGridOnChangedEvent event) {
+            print(event);
+          },
+          configuration: const PlutoGridConfiguration(),
+        ),
       ),
     );
   }
